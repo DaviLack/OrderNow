6be74add5e1ee0b1653fcd6288b1d9d18e92e08e
KelvinNatal <kelvinnatal@hotmail.com>
2019-08-13 17:38:28 -0300

    Cu

---
+++ app/src/main/java/com/example/ordernow/TesteListView/PostsListActivity.java
@@ -0,0 +1,15 @@
+package com.example.ordernow.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ordernow.R;
+
+public class PostsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posts_list);
+    }
+}
