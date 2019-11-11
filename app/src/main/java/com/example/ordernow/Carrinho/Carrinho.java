ffb54ebb218dbc7c9de371ef7f95899b6312034f
KelvinNatal <kelvinnatal@hotmail.com>
2019-11-11 20:33:26 -0300

    Perfil e cartao

--- app/src/main/java/com/example/ordernow/Carrinho/Carrinho.java
+++ app/src/main/java/com/example/ordernow/Carrinho/Carrinho.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ordernow.Activitys.FinalActivity;
+import com.example.ordernow.Activitys.Payment;
+import com.example.ordernow.Activitys.ProfileActivity;
 import com.example.ordernow.Adapters.CartAdapter;
 import com.example.ordernow.Database.Database;
 import com.example.ordernow.R;
@@ -67,7 +71,9 @@ public class Carrinho extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                showAlertDialog();
+                startActivity(new Intent(getApplicationContext(), FinalActivity.class));
+
+                // showAlertDialog();
             }
         });
 
